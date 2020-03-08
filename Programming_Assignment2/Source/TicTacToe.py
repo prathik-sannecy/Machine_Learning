7485a3c7b3848e1d388ba5a2ceb905fd3c079ae0
prathik <prathik@pdx.edu>
2020-03-07 20:39:21 -0800

    Changed Tic Tac Toe game into an object Added further functions and tests for playing Tic Tac Toe -Check valid move -Check grid full -Make move

--- Programming_Assignment2/Source/TicTacToe.py
+++ Programming_Assignment2/Source/TicTacToe.py
@@ -1,4 +1,78 @@
-def display_grid(game_state):
-    for row in game_state:
-        grid_row = [' ' if e is None else e for e in row]
-        print('|'.join(grid_row))
+import numpy
+
+class TicTacToeGame():
+
+    def __init__(self):
+        self.game_state = [[None]*3 for _ in range(3)]
+
+    def display_grid(self):
+        """Prints Tic Tac Toe grid
+    
+        inputs:
+            None
+    
+        returns:
+            None
+        """
+        for row in self.game_state:
+            grid_row = [' ' if e is None else e for e in row]
+            print('|'.join(grid_row))
+    
+    def test_valid_move(self, move):
+        """Tests if a tic tack toe move is valid or not
+    
+        inputs:
+            list[int, int] move: the grid location of the next move
+    
+        returns:
+            True if the move is valid, False otherwise
+        """
+        if self.game_state[move[0]][move[1]] is not None:
+            return False
+        return True
+    
+    def check_win(self, player):
+        """Checks whether a player has won or not
+    
+        inputs:
+            (char) player: which player to check has wone
+    
+        returns:
+            True if the player has won, False otherwise
+        """
+        pass
+    
+    
+    def check_full(self):
+        """Checks whether the grid is full
+    
+        inputs:
+            None
+    
+        returns:
+            True if the grid is full (no empty spots), False otherwise
+        """
+        for row in self.game_state:
+            for e in row:
+                if e is None:
+                    return False
+        return True
+    
+    def make_move(self, move, player):
+        """A player makes a move on the grid
+    
+        inputs:
+            (char) player: the player making the move
+            list[int, int] move: the grid location of the players move
+    
+        returns:
+            False if the move is not valid, or the new updated grid with the player's move
+        """
+        if not self.test_valid_move( move):
+            return False
+        self.game_state[move[0]][move[1]] = player
+
+    def get_game_state(self):
+        return self.game_state
+    
+    
--- Programming_Assignment2/Tests/test_TicTacToe.py
+++ Programming_Assignment2/Tests/test_TicTacToe.py
@@ -3,13 +3,58 @@ from Programming_Assignment2.Source.TicTacToe import *
 
 
 class test_TicTacToe(unittest.TestCase):
+
     def test_display_grid(self):
-        grid = [
+        tic_tack_toe = TicTacToeGame()
+        tic_tack_toe.game_state = [
             ['X', 'O', None],
             ['O', 'O', "X"],
             [None, 'X', None]
         ]
-        display_grid(grid)
+        tic_tack_toe.display_grid()
+
+    def test_test_valid_move(self):
+        tic_tack_toe = TicTacToeGame()
+        tic_tack_toe.game_state = [
+            ['X', 'O', None],
+            ['O', 'O', "X"],
+            [None, 'X', None]
+        ]
+        assert(tic_tack_toe.test_valid_move((0, 2)) == True)
+        assert(tic_tack_toe.test_valid_move((1, 1)) == False)
+
+    def test_check_full(self):
+        tic_tack_toe = TicTacToeGame()
+        tic_tack_toe.game_state = [
+            ['X', 'O', None],
+            ['O', 'O', "X"],
+            [None, 'X', None]
+        ]
+        assert(tic_tack_toe.check_full() == False)
+        tic_tack_toe.game_state = [
+            ['X', 'O', 'X'],
+            ['O', 'O', "X"],
+            ['O', 'X', 'O']
+        ]
+        assert(tic_tack_toe.check_full() == True)
+
+    def test_make_move(self):
+        tic_tack_toe = TicTacToeGame()
+        tic_tack_toe.game_state = [
+            ['X', 'O', None],
+            ['O', 'O', "X"],
+            [None, 'X', None]
+        ]
+        assert(tic_tack_toe.make_move([1,0], 'X') == False)
+        grid_new = [
+            ['X', 'O', None],
+            ['O', 'O', "X"],
+            ['X', 'X', None]
+        ]
+        tic_tack_toe.make_move([2, 0], 'X')
+        assert(tic_tack_toe.get_game_state() == grid_new)
+
+
 
 
 if __name__ == '__main__':
