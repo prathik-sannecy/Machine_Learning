4f5b8063b200093c695f1ba5f76296633116131f
prathik <prathik@pdx.edu>
2020-03-08 01:30:30 -0800

    Added function and test to QTable for getting the best action (based on rewards) for when the Tic Tac Toe is in a particular state

--- Programming_Assignment2/Source/QLearning.py
+++ Programming_Assignment2/Source/QLearning.py
@@ -1,28 +1,55 @@
 import Programming_Assignment2.Source.TicTacToe as TicTacToe
 import itertools
 
-def init_QTable_TicTacToe():
-    """Initializes the QTable for TicTacToe with all game states and actions = 0
+class QTable_TicTacToe():
+    def __init__(self):
+        """Initializes the QTable for TicTacToe with all game states and actions = 0
 
-    inputs:
-        None
+        inputs:
+            None
 
-    returns:
-        List[List[List[List[char]],List[int, int]], int] QTable: Each [game state, move] maps to 0
-    """
-    QTable = []
-    row = [None, 'X', 'O']
-    grid_offset = [0, 1, 2]
-    row_combinations = list(map(list, itertools.product(row, repeat=3))) # All possible products of a row (eg [0, None, None])
-    grid_locations = list(map(list, itertools.product(grid_offset, repeat=2))) # All possible grid locations (eg [0, 2])
-    # Generate every possible grid, and initialize the QTable for that grid=0
-    for row1 in row_combinations:
-        for row2 in row_combinations:
-            for row3 in row_combinations:
-                for location in grid_locations:
-                    QTable.append([[row1, row2, row3], location, 0])
-    return QTable
-
-
+        returns:
+            None
+        """
+        self.QTable = []
+        row = [None, 'X', 'O']
+        grid_offset = [0, 1, 2]
+        row_combinations = list(map(list, itertools.product(row, repeat=3))) # All possible products of a row (eg [0, None, None])
+        grid_locations = list(map(list, itertools.product(grid_offset, repeat=2))) # All possible grid locations (eg [0, 2])
+        # Generate every possible grid, and initialize the QTable for that grid=0
+        for row1 in row_combinations:
+            for row2 in row_combinations:
+                for row3 in row_combinations:
+                    for location in grid_locations:
+                        self.QTable.append([[row1, row2, row3], location, 0])
 
 
+    def get_actions_rewards(self, tic_tac_toe_game:TicTacToe.TicTacToeGame):
+        """Returns all the state's actions that are valid and their respective rewards
+
+        inputs:
+            TicTacToeGame tic_tac_toe_game: the current state of the tic tac toe game
+
+        returns:
+            List[List[List[int, int], int]] action_rewards: all the possible actions from a particular state, and their respective rewards
+        """
+        return [[action, value] for [state, action, value] in self.QTable if state == tic_tac_toe_game.get_game_state() and tic_tac_toe_game.test_valid_move(action)]
+
+    def max_action_reward(self, tic_tac_toe_game:TicTacToe.TicTacToeGame ):
+        """Returns the action and the value that has the largest reward based on being in a particular state
+
+        inputs:
+            TicTacToeGame tic_tac_toe_game: the current state of the tic tac toe game
+
+        returns:
+            List[List[int, int], int] max_action_reward: the action and reward with that's the largest based on the current state
+        """
+        # Traverse through all the possible actions in a particular state
+        # Pick the action that yields the highest reward
+        actions_values = self.get_actions_rewards(tic_tac_toe_game)
+        [max_action, max_value] = actions_values[0]
+        for action_value in actions_values:
+            [action, value] = action_value
+            if value > max_value:
+                max_action, max_value = action, value
+        return [max_action, max_value]
--- Programming_Assignment2/Tests/test_QLearning.py
+++ Programming_Assignment2/Tests/test_QLearning.py
@@ -1,11 +1,11 @@
 import unittest
 from Programming_Assignment2.Source.QLearning import *
-
+from Programming_Assignment2.Source.TicTacToe import *
 
 class test_QLearning(unittest.TestCase):
 
     def test_init_QTable(self):
-        QTable_init = init_QTable_TicTacToe()
+        QTable = QTable_TicTacToe()
         grid = [
             ['X', 'O', None],
             ['O', 'O', 'X'],
@@ -14,7 +14,7 @@ class test_QLearning(unittest.TestCase):
         location = [0, 2]
 
         # Make sure the grid is intialized in the QTable, and only one instance is there
-        mapped_grid = [match[2] for match in QTable_init if match[0] == grid and match[1] == location]
+        mapped_grid = [match[2] for match in QTable.QTable if match[0] == grid and match[1] == location]
         assert(mapped_grid == [0])
 
         grid = [
@@ -24,10 +24,34 @@ class test_QLearning(unittest.TestCase):
         ]
         location = [0, 0]
         # Make sure the grid is intialized in the QTable, and only one instance is there
-        mapped_grid = [match[2] for match in QTable_init if match[0] == grid and match[1] == location]
+        mapped_grid = [match[2] for match in QTable.QTable if match[0] == grid and match[1] == location]
         assert(mapped_grid == [0])
 
-        assert(len(QTable_init) == 177147) # 27*27*27 different grids x 9 different grid locations
+        assert(len(QTable.QTable) == 177147) # 27*27*27 different grids x 9 different grid locations
+
+    def test_get_actions_rewards(self):
+        tic_tack_toe = TicTacToeGame()
+        tic_tack_toe.game_state = [
+            ['X', 'O', None],
+            ['O', 'O', "X"],
+            [None, 'X', None]
+        ]
+        QTable = QTable_TicTacToe()
+        assert(QTable.get_actions_rewards(tic_tack_toe) == [[[0, 2], 0], [[2, 0], 0], [[2, 2], 0]])
+
+    def test_max_action_reward(self):
+        tic_tack_toe = TicTacToeGame()
+        tic_tack_toe.game_state = [
+            ['X', 'O', None],
+            ['O', 'O', "X"],
+            [None, 'X', None]
+        ]
+        QTable = QTable_TicTacToe()
+        for e in QTable.QTable:
+            if e[0] == tic_tack_toe.game_state and e[1] == [2, 2]:
+                e[2] = 4
+        assert(QTable.max_action_reward(tic_tack_toe) == [[2, 2], 4])
+
 
 
 if __name__ == '__main__':
